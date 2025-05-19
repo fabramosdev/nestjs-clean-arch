53ff7c5dc09d7bd2c236ba749c76947d4c8e402d
Fabiano S. Ramos <fabianosantosramos1978@gmail.com>
2025-05-19 13:58:09 -0300

    refactor: env config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
+import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
 
 @Module({
-  imports: [EnvConfigModule],
+  imports: [EnvConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
